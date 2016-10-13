ea49f9d47ca86e0adc487dd0ab5b7833c8f352d6
marcelbuesing <buesing.marcel@googlemail.com>
2016-10-13 21:53:16 +0200

    Add pipeline event type

--- src/GitlabHooks/Data/Types.hs
+++ src/GitlabHooks/Data/Types.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString.Char8 as B
 import Data.Aeson
 import Data.Aeson.Types (Parser, typeMismatch)
 import qualified Data.HashMap.Lazy as HML
+import Data.Maybe (fromJust)
 import Data.Text as T
 import Network.URL (URL(..), importURL)
 import Text.Email.Validate (EmailAddress, emailAddress)
@@ -45,14 +46,23 @@ data GitlabEvent =
   , _issueEventUser :: User
   , _issueEventProject :: Project
   , _issueEventRepository :: IssueEventRepository
-  , _issueEventObjectAttributes :: ObjectAttribute
+  , _issueEventObjectAttributes :: IssueEventObjectAttribute
   , _issueEventAssignee :: Assignee
+  }
+  | PipelineEvent
+  { _pipelineEventObjectKind :: Text
+  , _pipelineEventObjectAttributes :: PipelineEventObjectAttribute
+  , _pipelineEventUser :: User
+  , _pipelineEventProject :: PipelineEventProject
+  , _pipelineEventCommit :: PipelineEventCommit
+  , _pipelineEventBuilds :: [Build]
   } deriving Show
 
 instance FromJSON GitlabEvent where
   parseJSON (Object v) = case HML.lookup "object_kind" v of
     Just (String "push") -> parsePushEvent v
     Just (String "issue") -> parseIssueEvent v
+    Just (String "pipeline") -> parsePipelineEvent v
     _ -> fail "unexpected event"
 
 parsePushEvent :: Object -> Parser GitlabEvent
@@ -80,6 +90,15 @@ parseIssueEvent v = IssueEvent <$>
     v .: "repository" <*>
     v .: "object_attributes" <*>
     v .: "assignee"
+
+parsePipelineEvent :: Object -> Parser GitlabEvent
+parsePipelineEvent v = PipelineEvent <$>
+  v .: "object_kind" <*>
+  v .: "object_attributes" <*>
+  v .: "user" <*>
+  v .: "project" <*>
+  v .: "commit" <*>
+  v .: "builds"
 
 type CommitRef = Text
 type ProjectId = Int
@@ -218,7 +237,7 @@ type ObjectAttributeState = Text
 type ObjectAttributeIid = Int
 type ObjectAttributeAction = Text
 
-data ObjectAttribute = ObjectAttribute
+data IssueEventObjectAttribute = IssueEventObjectAttribute
   { _objectAttributeId :: ObjectAttributeId
   , _objectAttributeTitle :: Text
   , _objectAttributeAssigneeId :: AssigneeId
@@ -236,8 +255,8 @@ data ObjectAttribute = ObjectAttribute
   , _objectAttributeAction :: ObjectAttributeAction
   } deriving Show
 
-instance FromJSON ObjectAttribute where
-  parseJSON (Object v) = ObjectAttribute <$>
+instance FromJSON IssueEventObjectAttribute where
+  parseJSON (Object v) = IssueEventObjectAttribute <$>
     v .: "id" <*>
     v .: "title" <*>
     v .: "assignee_id" <*>
@@ -253,3 +272,126 @@ instance FromJSON ObjectAttribute where
     v .: "iid" <*>
     v .: "url" <*>
     v .: "action"
+
+type ObjectAttributeRef = Text
+type SHA = Text
+type PipelineEventStatus = Text
+type PipelineEventStage = Text
+
+type GitlabDate = Text
+
+data PipelineEventObjectAttribute = PipelineEventObjectAttribute
+  { _pipelineEventObjectAttributeId :: ObjectAttributeId
+  , _pipelineEventObjectAttributeRef :: ObjectAttributeRef
+  , _pipelineEventObjectAuttributeTag :: Bool
+  , _pipelineEventObjectAttributeSha :: SHA
+  , _pipelineEventObjectAttributeBeforeSHA :: SHA
+  , _pipelineEventObjectAttributeStatus :: PipelineEventStatus
+  , _pipelineEventObjectAttributeStages :: [PipelineEventStage]
+  , _pipelineEventObjectAttributeCreatedAt :: GitlabDate
+  , _pipelineEventObjectAttributeFinishedAt :: GitlabDate
+  , _pipelineEventObjectAttributeDuration :: Int
+  } deriving Show
+
+instance FromJSON PipelineEventObjectAttribute where
+  parseJSON (Object v) = PipelineEventObjectAttribute <$>
+    v .: "id" <*>
+    v .: "ref" <*>
+    v .: "tag" <*>
+    v .: "sha" <*>
+    v .: "before_sha" <*>
+    v .: "status" <*>
+    v .: "stages" <*>
+    v .: "created_at" <*>
+    v .: "finished_at" <*>
+    v .: "duration"
+
+data PipelineEventProject = PipelineEventProject
+  { _pipelineEventProjectName :: Text
+  , _pipelineEventProjectDescription :: Text
+  , _pipelineEventProjectWebUrl :: URL
+  , _pipelineEventProjectAvatarUrl :: Maybe URL
+  , _pipelineEventProjectGitSshUrl :: URL
+  , _pipelineEventProjectGitHttpUrl :: URL
+  , _pipelineEventProjectNamespace :: Text
+  , _pipelineEventProjectVisibilitylevel :: Int
+  , _pipelineEventProjectPathWithNamespace :: Text
+  , _pipelineEventProjectDefaultBranch :: Text
+  } deriving Show
+
+instance FromJSON PipelineEventProject where
+  parseJSON (Object v) = PipelineEventProject <$>
+    v .: "name" <*>
+    v .: "description" <*>
+    v .: "web_url" <*>
+    v .: "avatar_url" <*>
+    v .: "git_ssh_url" <*>
+    v .: "git_http_url" <*>
+    v .: "namespace" <*>
+    v .: "visibility_level" <*>
+    v .: "path_with_namespace" <*>
+    v .: "default_branch"
+
+data PipelineEventCommit = PipelineEventCommit
+  { _pipeLineEventCommitId :: CommitRef
+  , _pipeLineEventCommitMessage :: Text
+  , _pipeLineEventCommitTimestamp :: Text
+  , _pipeLineEventCommitUrl :: URL
+  , _pipeLineEventCommitAuthor :: CommitAuthor
+  } deriving Show
+
+instance FromJSON PipelineEventCommit where
+  parseJSON (Object v) = PipelineEventCommit <$>
+    v .: "id" <*>
+    v .: "message" <*>
+    v .: "timestamp" <*>
+    v .: "url" <*>
+    v .: "author"
+
+type BuildId = Int
+type BuildStage = Text
+type BuildRunner = Text
+type BuildStatus = Text
+
+data Build = Build
+  { _buildId :: BuildId
+  , _buildStage :: BuildStage
+  , _buildName :: Text
+  , _buildStatus :: BuildStatus
+  , _buildCreatedAt :: GitlabDate
+  , _buildStartedAt :: Maybe GitlabDate
+  , _buildFinishedAt :: Maybe GitlabDate
+  , _buildWhen :: Text
+  , _buildManual :: Bool
+  , _buildUser :: User
+  , _buildRunner :: Maybe BuildRunner
+  , _buildArtifactsFile :: BuildArtifactsFile
+  } deriving Show
+
+instance FromJSON Build where
+  parseJSON (Object v) = Build <$>
+    v .: "id" <*>
+    v .: "stage" <*>
+    v .: "name" <*>
+    v .: "status" <*>
+    v .: "created_at" <*>
+    v .: "started_at" <*>
+    v .: "finished_at" <*>
+    v .: "when" <*>
+    v .: "manual" <*>
+    v .: "user" <*>
+    v .: "runner" <*>
+    v .: "artifacts_file"
+
+type FileName = Text
+type FileSize = Int
+
+data BuildArtifactsFile = BuildArtifactsFile
+  { _buildArtifactsFileFileName :: Maybe FileName
+  , _buildArtifactsFileSize :: Maybe FileSize
+  } deriving Show
+
+instance FromJSON BuildArtifactsFile where
+  parseJSON (Object v) = BuildArtifactsFile <$>
+    v .: "filename" <*>
+    v .: "size"

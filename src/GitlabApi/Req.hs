431c775b411c508b40a61f0e38718e3e144f6f3b
marcelbuesing <buesing.marcel@googlemail.com>
2018-10-23 08:14:43 +0200

    Reference GitlabApi.Req.Common module

--- src/GitlabApi/Req.hs
+++ src/GitlabApi/Req.hs
@@ -1,7 +1,9 @@
 module GitlabApi.Req (
-    module GitlabApi.Req.Groups
+    module GitlabApi.Req.Common
+  , module GitlabApi.Req.Groups
   , module GitlabApi.Req.Services
   ) where
 
+import GitlabApi.Req.Common
 import GitlabApi.Req.Groups
 import GitlabApi.Req.Services
